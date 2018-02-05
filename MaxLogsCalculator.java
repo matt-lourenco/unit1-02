2ce29bb5fcadd468137cf03d9d07d947bd7c294e
Matt <matthew.lourenco@ocsbstudent.ca>
2018-02-05 13:25:35 -0500

    Add files via upload

---
+++ MaxLogsCalculator.java
@@ -0,0 +1,18 @@
+/****************************************************************************
+ *
+ * Created by: Matthew lourenco
+ * Created on: Feb 2018
+ * This program calculates the number of logs that will fit on a truck,
+ *     if you know their length
+ *
+ ****************************************************************************/
+
+public class MaxLogsCalculator {
+	
+	public static void main(String[] args) {
+		//Calculate the max number of logs
+		float lengthOfLog = 0.25f;
+		int maxWeightCapacity = 1100;
+		int WeightPerMetre = 20;
+	}
+}
