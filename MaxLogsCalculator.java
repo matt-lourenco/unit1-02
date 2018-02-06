d5a83e9759e2cee5c017e0083897850a50dcabce
Matt <matthew.lourenco@ocsbstudent.ca>
2018-02-05 21:06:47 -0500

    Update MaxLogsCalculator.java

--- MaxLogsCalculator.java
+++ MaxLogsCalculator.java
@@ -7,12 +7,41 @@
  *
  ****************************************************************************/
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MaxLogsCalculator {
 	
 	public static void main(String[] args) {
 		//Calculate the max number of logs
-		float lengthOfLog = 0.25f;
-		int maxWeightCapacity = 1100;
-		int WeightPerMetre = 20;
+		String input = null;
+		double lengthOfLog = 0.0;
+		double maxWeightCapacity = 1100;
+		double weightPerMetre = 20;
+		double maxTotalLength = maxWeightCapacity / weightPerMetre;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader
+				(System.in));
+		while(true) {
+			System.out.println("Enter the length of the logs: ");
+			try {
+				//Get user input
+				input = reader.readLine();
+			} catch (IOException noInput) {
+				noInput.printStackTrace();
+			}
+			try {
+				//Convert string to double
+				lengthOfLog = Double.parseDouble(input);
+				break;
+			} catch (NumberFormatException stringInput) {
+				System.out.println("Please enter a number");
+			}
+		}
+		
+		double maxNumberOfLogs = maxTotalLength / lengthOfLog;
+		System.out.println("A maximum of " + maxNumberOfLogs + " logs can "
+				+ "fit on the truck.");
 	}
 }
